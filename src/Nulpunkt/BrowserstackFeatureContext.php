fb82219d5fe8357a1cb8cf2b7628d3f72ddf6a59
Jesper Skovgaard Nielsen <jesper@runlevel0.dk>
2014-02-13 10:32:44 +0100

    Created Browserstack aware context

---
+++ src/Nulpunkt/BrowserstackFeatureContext.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Nulpunkt;
+
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use Behat\Mink\Driver\Selenium2Driver;
+
+class BrowserstackFeatureContext extends Behat\MinkExtension\Context\MinkContext
+{
+    public function __construct($params)
+    {
+        $this->setBrowserstackParams($params);
+
+        $host = "http://{$this->browserstack_username}:{$this->browserstack_password}@hub.browserstack.com/wd/hub";
+        $mink = new Mink(array(
+            'selenium2' => new Session(new Selenium2Driver('', $this->capabilities, $host)),
+        ));
+
+        $mink->setDefaultSessionName('selenium2');
+        $this->setMink($mink);
+        $this->setMinkParameters($params);
+    }
+
+    private function setBrowserstackParams($params)
+    {
+        if (!isset($params['username'])) {
+            throw new \Exception("Please specify your browserstack username as a parameter to the feature context in behat.yml");
+        }
+        if (!isset($params['password'])) {
+            throw new \Exception("Please specify your browserstack password as a parameter to the feature context in behat.yml");
+        }
+
+        $this->browserstack_username = $params['username'];
+        $this->browserstack_password = $params['password'];
+        $this->capabilities = isset($params['capabilities']) ? $params['capabilities'] : null;
+    }
+}
